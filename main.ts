c7aff5173fe8b54c6700868d9827ab9839f7aec0
jorolfus <69719767+jorolfus@users.noreply.github.com>
2020-08-16 15:11:52 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -36,12 +36,13 @@ let dx = 1, dy = 1
 let x = 0
 let y = 0 
 forever(function () {
+    info.changeScoreBy(1)
     x = x + dx
     y = y + dy 
-    if (x >= 159) dx = -dx
-    if (y >= 119) dy = -dy            
-    if (x <= 0) dx = -dx
-    if (y <= 0) dy = -dy  
+    if (x >= 159) dx = -Math.random()*2
+    if (y >= 119) dy = -Math.random()*2           
+    if (x <= 0) dx = Math.random()*2
+    if (y <= 0) dy = Math.random() *2
  P.setPosition(x, y )
 let projectile = sprites.createProjectileFromSprite(img`
     . . 2 . .
@@ -49,5 +50,30 @@ let projectile = sprites.createProjectileFromSprite(img`
     2 2 . 2 2
     . 2 2 2 .
     . . 2 . .
-`, P,dx* -100,dy* -100)
+`, P,dx*-100,dy*-100)
+})
+let A = sprites.create(img`
+    . . . 9 9 9 9 9 9 9 9 9 . . . .
+    . . . 9 . 1 . . 1 . 9 9 . . . .
+    . 9 9 9 9 9 9 9 9 9 9 9 9 9 . .
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 .
+    9 9 9 9 9 9 9 9 9 9 9 . 9 9 9 9
+    9 9 . 9 9 9 9 9 9 9 9 . . 9 9 9
+    9 . . 9 9 9 9 9 9 9 9 . . . 9 9
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+    . . 2 2 2 . . 2 2 2 . . . . . .
+`,SpriteKind.Player)
+controller.moveSprite(A)
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function(sprite: Sprite, otherSprite: Sprite) {
+    sprites.allOfKind(SpriteKind.Projectile).forEach(function(value: Sprite, index: number) {
+     value.destroy()   
+    })
+    info.changeLifeBy(-1)
 })
